0de059020e9d80dadc658f62c07cd64e7d8a9391
Josha Huybrechts <s131625@ap.be>
2023-01-08 21:24:07 +0100

    Finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -52,10 +52,14 @@ const IndexPage = ({
       </section>
       <section className={form}>
         <form className={contactForm} name="contact" method="POST" data-netlify="true">
-            <label>Your Name:</label>
-            <input type="text" name="name" required={true} />
-            <label>Your Email:</label>
+            <label>First name:</label>
+            <input type="text" name="firstname" required={true} />
+            <label>Last name:</label>
+            <input type="text" name="lastname" required={true} />
+            <label>Email:</label>
             <input type="email" name="email" required={true} />
+            <label>Subject:</label>
+            <input type="text" name="subject" required={true} />
             <label>Message:</label>
             <textarea name="message" required={true}></textarea>
             <input type="hidden" name="form-name" value="contact" />
